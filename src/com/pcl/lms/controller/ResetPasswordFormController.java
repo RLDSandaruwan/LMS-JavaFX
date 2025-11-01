243cdb96b19a32e7255751dd7180723ed2372d49
RLDSandaruwan <dahamsandr@gmail.com>
2025-11-01 18:55:57 +0530

    password reset feature done

--- src/com/pcl/lms/controller/ResetPasswordFormController.java
+++ src/com/pcl/lms/controller/ResetPasswordFormController.java
@@ -1,31 +1,60 @@
 package com.pcl.lms.controller;
 
+import com.pcl.lms.DB.Database;
+import com.pcl.lms.env.StaticResource;
 import com.pcl.lms.model.User;
+import com.pcl.lms.utill.security.PasswordManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ResetPasswordFormController {
     public AnchorPane context;
+    public Label lblCompany;
+    public Label lblVersion;
+    public PasswordField txtNewPassword;
+    public PasswordField txtConfirmPassword;
     private String email;
+
+    public void initialize() {
+        setStaticData();
+    }
+
+    private void setStaticData() {
+        lblCompany.setText(StaticResource.getCOMPANY());
+        lblVersion.setText(StaticResource.getVERSION());
+    }
 
     public void setUserData(String email){
         this.email=email;
     }
-    public User user;
 
     public void navigateOtpVerificationForm(ActionEvent actionEvent) throws IOException {
-        setUi("VerifyOTPForm");
-    }
-
-    public void resetPasswordOnAction(ActionEvent actionEvent) throws IOException {
         setUi("LoginForm");
     }
 
+    public void resetPasswordOnAction(ActionEvent actionEvent) throws IOException {
+        Optional <User> selectedUser = Database.userTable.stream().filter(u -> u.getEmail().equals(email)).findFirst();
+        if(selectedUser.isPresent()){
+            if(txtNewPassword.getText().trim().equals(txtConfirmPassword.getText().trim())){
+                selectedUser.get().setPassword(new PasswordManager().encode(txtNewPassword.getText().trim()));
+                System.out.println(selectedUser.get().getPassword());
+                new Alert(Alert.AlertType.INFORMATION,"Password Reset Successfully!").show();
+                setUi("LoginForm");
+            }else {
+                new Alert(Alert.AlertType.INFORMATION,"Passwords are dont match").show();
+            }
+        }
+
+    }
 
     private void setUi(String location) throws IOException {
         Stage stage =(Stage) context.getScene().getWindow();
